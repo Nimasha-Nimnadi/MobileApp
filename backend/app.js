ed351ecfb15caec47e67f73a50d800464deac6bb
Ishan Senanayaka <120593815+Mishan20@users.noreply.github.com>
2023-12-01 23:46:29 +0530

    Delete backend directory

--- backend/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const body_parser = require('body-parser');
-const UserRouter = require('./routes/user_router');
-
-const app =express();
-app.use(body_parser.json());
-app.use('/',UserRouter);
-module.exports = app;
--- backend/routes/user_router.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const userController = require('../controller/user_controller');
-
-
-router.post('/registration',userController.register);
-
-module.exports =router;
